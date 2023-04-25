2c7dea2d2ecee006cfe321461780d2ff9ec7e82d
Danny <raducudaniel05@gmail.com>
2023-04-25 09:23:01 +0200

    HoneyBee Vererbung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,19 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
+        output(queen.fly());
+       
+        output(worker.doYourJob());
+        output(worker.fly());
+        
+        output(drone.doYourJob());
+        output(drone.fly());
      
         
     }
